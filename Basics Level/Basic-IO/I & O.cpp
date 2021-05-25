7356ef5262074c635be96a0cb8626c09b55aaaeb
mwaqasnazir <wbutt1514@gmail.com>
2021-05-25 22:51:54 +0500

    Day 1 of 100daysofcode

---
+++ Basics Level/Basic-IO/I & O.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+ main() {
+   std::string fn,ln;
+   
+   std::cout <<"Enter Your First Name:";
+   std::cin>>fn;
+   
+   std::cout <<"Enter Your Last Name:";
+   std::cin>>ln;
+   
+   std::cout<<"Your Full Name is : "<<fn<<" "<<ln;
+   
+   return 0;
+}
+
+
+
+
+
+
+
